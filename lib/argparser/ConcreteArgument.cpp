b11a6363b2439aa9695374f7d219911bf99acdd3
bialger <artur.bigulov@yandex.ru>
2024-02-07 13:45:27 +0300

    Moved all non-specialized functions of ConcreteArgument to header

--- lib/argparser/ConcreteArgument.cpp
+++ lib/argparser/ConcreteArgument.cpp
@@ -29,106 +29,6 @@ class ArgumentParser::ConcreteArgument<std::string>;
 template
 class ArgumentParser::ConcreteArgument<CompositeString>;
 
-template<typename T>
-T ArgumentParser::ConcreteArgument<T>::GetValue(size_t index) const {
-  if (!info_.has_store_values) {
-    return value_;
-  }
-
-  return stored_values_->at(index);
-}
-
-template<typename T>
-ArgumentParser::ArgumentParsingStatus ArgumentParser::ConcreteArgument<T>::GetValueStatus() const {
-  return value_status_;
-}
-
-template<typename T>
-const std::string& ArgumentParser::ConcreteArgument<T>::GetType() const {
-  return info_.type;
-}
-
-template<typename T>
-const ArgumentParser::ArgumentInformation& ArgumentParser::ConcreteArgument<T>::GetInfo() const {
-  return info_;
-}
-
-template<typename T>
-size_t ArgumentParser::ConcreteArgument<T>::GetUsedValues() const {
-  return value_counter_;
-}
-
-template<typename T>
-bool ArgumentParser::ConcreteArgument<T>::CheckLimit() {
-  if (value_counter_ < info_.minimum_values) {
-    value_status_ = ArgumentParsingStatus::kInsufficientArguments;
-    return false;
-  }
-
-  if (value_status_ == ArgumentParsingStatus::kInsufficientArguments) {
-    value_status_ = ArgumentParsingStatus::kSuccess;
-  }
-
-  return true;
-}
-
-template<typename T>
-void ArgumentParser::ConcreteArgument<T>::ClearStored() {
-  stored_values_->clear();
-  value_counter_ = 0;
-
-  if (stored_value_ != nullptr) {
-    *stored_value_ = default_value_;
-  }
-}
-
-template<typename T>
-std::vector<size_t> ArgumentParser::ConcreteArgument<T>::ValidateArgument(const std::vector<std::string>& argv,
-                                                                          size_t position) {
-  std::vector<size_t> used_positions;
-  if (stored_values_->empty()) {
-    value_status_ = ArgumentParsingStatus::kSuccess;
-  }
-
-  while (used_positions.empty() || info_.is_multi_value) {
-    std::string value_string = argv[position];
-    used_positions.push_back(position);
-
-    if (argv[position][0] == '-') {
-      size_t equals_index = argv[position].find('=');
-
-      if (equals_index != std::string::npos) {
-        value_string = argv[position].substr(equals_index + 1);
-      } else if (std::is_same<T, bool>::value) {
-        value_string = "true";
-      } else {
-        ++position;
-        used_positions.push_back(position);
-        value_string = argv[position];
-      }
-    }
-
-    position = ObtainValue(argv, value_string, used_positions, position);
-
-    ++position;
-    ++value_counter_;
-
-    stored_values_->push_back(value_);
-
-    if (position >= argv.size() || argv[position][0] == '-') {
-      break;
-    }
-  }
-
-  CheckLimit();
-
-  if (info_.has_store_value) {
-    *stored_value_ = value_;
-  }
-
-  return used_positions;
-}
-
 template<>
 size_t ArgumentParser::ConcreteArgument<int16_t>::ObtainValue(const std::vector<std::string>& argv,
                                                               std::string& value_string,
@@ -197,9 +97,9 @@ size_t ArgumentParser::ConcreteArgument<int64_t>::ObtainValue(const std::vector<
 
 template<>
 size_t ArgumentParser::ConcreteArgument<uint16_t>::ObtainValue(const std::vector<std::string>& argv,
-                                                              std::string& value_string,
-                                                              std::vector<size_t>& used_values,
-                                                              size_t position) {
+                                                               std::string& value_string,
+                                                               std::vector<size_t>& used_values,
+                                                               size_t position) {
   int32_t save_errno = errno;
   errno = 0;
   char* end;
@@ -219,9 +119,9 @@ size_t ArgumentParser::ConcreteArgument<uint16_t>::ObtainValue(const std::vector
 
 template<>
 size_t ArgumentParser::ConcreteArgument<uint32_t>::ObtainValue(const std::vector<std::string>& argv,
-                                                              std::string& value_string,
-                                                              std::vector<size_t>& used_values,
-                                                              size_t position) {
+                                                               std::string& value_string,
+                                                               std::vector<size_t>& used_values,
+                                                               size_t position) {
   int32_t save_errno = errno;
   errno = 0;
   char* end;
@@ -241,9 +141,9 @@ size_t ArgumentParser::ConcreteArgument<uint32_t>::ObtainValue(const std::vector
 
 template<>
 size_t ArgumentParser::ConcreteArgument<uint64_t>::ObtainValue(const std::vector<std::string>& argv,
-                                                              std::string& value_string,
-                                                              std::vector<size_t>& used_values,
-                                                              size_t position) {
+                                                               std::string& value_string,
+                                                               std::vector<size_t>& used_values,
+                                                               size_t position) {
   int32_t save_errno = errno;
   errno = 0;
   char* end;
@@ -263,9 +163,9 @@ size_t ArgumentParser::ConcreteArgument<uint64_t>::ObtainValue(const std::vector
 
 template<>
 size_t ArgumentParser::ConcreteArgument<float>::ObtainValue(const std::vector<std::string>& argv,
-                                                             std::string& value_string,
-                                                             std::vector<size_t>& used_values,
-                                                             size_t position) {
+                                                            std::string& value_string,
+                                                            std::vector<size_t>& used_values,
+                                                            size_t position) {
   int32_t save_errno = errno;
   errno = 0;
   char* end;
@@ -285,9 +185,9 @@ size_t ArgumentParser::ConcreteArgument<float>::ObtainValue(const std::vector<st
 
 template<>
 size_t ArgumentParser::ConcreteArgument<double>::ObtainValue(const std::vector<std::string>& argv,
-                                                                  std::string& value_string,
-                                                                  std::vector<size_t>& used_values,
-                                                                  size_t position) {
+                                                             std::string& value_string,
+                                                             std::vector<size_t>& used_values,
+                                                             size_t position) {
   int32_t save_errno = errno;
   errno = 0;
   char* end;
@@ -307,9 +207,9 @@ size_t ArgumentParser::ConcreteArgument<double>::ObtainValue(const std::vector<s
 
 template<>
 size_t ArgumentParser::ConcreteArgument<long double>::ObtainValue(const std::vector<std::string>& argv,
-                                                              std::string& value_string,
-                                                              std::vector<size_t>& used_values,
-                                                              size_t position) {
+                                                                  std::string& value_string,
+                                                                  std::vector<size_t>& used_values,
+                                                                  size_t position) {
   int32_t save_errno = errno;
   errno = 0;
   char* end;
@@ -345,9 +245,9 @@ size_t ArgumentParser::ConcreteArgument<bool>::ObtainValue(const std::vector<std
 
 template<>
 size_t ArgumentParser::ConcreteArgument<char>::ObtainValue(const std::vector<std::string>& argv,
-                                                              std::string& value_string,
-                                                              std::vector<size_t>& used_values,
-                                                              size_t position) {
+                                                           std::string& value_string,
+                                                           std::vector<size_t>& used_values,
+                                                           size_t position) {
   if (value_string[0] != '\\') {
     if (value_string.size() == 1 && info_.IsGood(value_string)) {
       value_ = value_string[0];
--- lib/argparser/ConcreteArgument.hpp
+++ lib/argparser/ConcreteArgument.hpp
@@ -44,6 +44,106 @@ ConcreteArgument<T>::ConcreteArgument(const ArgumentInformation& info,
   stored_values_ = stored_values;
 }
 
+template<typename T>
+T ConcreteArgument<T>::GetValue(size_t index) const {
+  if (!info_.has_store_values) {
+    return value_;
+  }
+
+  return stored_values_->at(index);
+}
+
+template<typename T>
+ArgumentParsingStatus ConcreteArgument<T>::GetValueStatus() const {
+  return value_status_;
+}
+
+template<typename T>
+const std::string& ConcreteArgument<T>::GetType() const {
+  return info_.type;
+}
+
+template<typename T>
+const ArgumentInformation& ConcreteArgument<T>::GetInfo() const {
+  return info_;
+}
+
+template<typename T>
+size_t ConcreteArgument<T>::GetUsedValues() const {
+  return value_counter_;
+}
+
+template<typename T>
+bool ConcreteArgument<T>::CheckLimit() {
+  if (value_counter_ < info_.minimum_values) {
+    value_status_ = ArgumentParsingStatus::kInsufficientArguments;
+    return false;
+  }
+
+  if (value_status_ == ArgumentParsingStatus::kInsufficientArguments) {
+    value_status_ = ArgumentParsingStatus::kSuccess;
+  }
+
+  return true;
+}
+
+template<typename T>
+void ConcreteArgument<T>::ClearStored() {
+  stored_values_->clear();
+  value_counter_ = 0;
+
+  if (stored_value_ != nullptr) {
+    *stored_value_ = default_value_;
+  }
+}
+
+template<typename T>
+std::vector<size_t> ConcreteArgument<T>::ValidateArgument(const std::vector<std::string>& argv,
+                                                                          size_t position) {
+  std::vector<size_t> used_positions;
+  if (stored_values_->empty()) {
+    value_status_ = ArgumentParsingStatus::kSuccess;
+  }
+
+  while (used_positions.empty() || info_.is_multi_value) {
+    std::string value_string = argv[position];
+    used_positions.push_back(position);
+
+    if (argv[position][0] == '-') {
+      size_t equals_index = argv[position].find('=');
+
+      if (equals_index != std::string::npos) {
+        value_string = argv[position].substr(equals_index + 1);
+      } else if (std::is_same<T, bool>::value) {
+        value_string = "true";
+      } else {
+        ++position;
+        used_positions.push_back(position);
+        value_string = argv[position];
+      }
+    }
+
+    position = ObtainValue(argv, value_string, used_positions, position);
+
+    ++position;
+    ++value_counter_;
+
+    stored_values_->push_back(value_);
+
+    if (position >= argv.size() || argv[position][0] == '-') {
+      break;
+    }
+  }
+
+  CheckLimit();
+
+  if (info_.has_store_value) {
+    *stored_value_ = value_;
+  }
+
+  return used_positions;
+}
+
 }
 
 #endif //CONCRETEARGUMENT_HPP_
